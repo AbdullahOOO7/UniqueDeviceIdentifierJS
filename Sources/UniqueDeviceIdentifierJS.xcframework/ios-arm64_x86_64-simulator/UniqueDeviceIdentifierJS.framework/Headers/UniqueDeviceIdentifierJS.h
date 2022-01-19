6a858e0e80c0625b07388f2ba6a51c05997a45b3
AbdullahOOO7 <77316984+AbdullahOOO7@users.noreply.github.com>
2022-01-19 22:38:00 +0500

    UniqueDeviceIdentiferJS initial commit

---
+++ Sources/UniqueDeviceIdentifierJS.xcframework/ios-arm64_x86_64-simulator/UniqueDeviceIdentifierJS.framework/Headers/UniqueDeviceIdentifierJS.h
@@ -0,0 +1,18 @@
+//
+//  UniqueDeviceIdentifierJS.h
+//  UniqueDeviceIdentifierJS
+//
+//  Created by Aksa on 19/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UniqueDeviceIdentifierJS.
+FOUNDATION_EXPORT double UniqueDeviceIdentifierJSVersionNumber;
+
+//! Project version string for UniqueDeviceIdentifierJS.
+FOUNDATION_EXPORT const unsigned char UniqueDeviceIdentifierJSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UniqueDeviceIdentifierJS/PublicHeader.h>
+
+
